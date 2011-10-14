9848303d11556d26afcb82c3b3f0c7d2822d411d
Nathan Ruska <nruska@gmail.com>
2011-10-13 21:09:54 -0700

    added name and email validation to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @attr = { :name => "Example User", :email => "user@example.com"}
+  end
+  
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+  
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid  
+ end
+ 
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+      
 end
+
+#with Rspec:
+#just adding it "something" assumes the call to the pending method
+
+#no_name_user.should_not be_valid is Rspec for:
+#no_name_user.valid?.should_not == true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
